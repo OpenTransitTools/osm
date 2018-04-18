e30d1a56cc659e7f011861a2af19806502d24c6b
Frank Purcell <fxpurcell@gmail.com>
2018-04-18 00:12:33 -0700

    more binary for osm2pgsql

--- ott/osm/osm2pgsql/osm2pgsql.py
+++ ott/osm/osm2pgsql/osm2pgsql.py
@@ -3,6 +3,7 @@ from ott.utils import file_utils
 
 
 import logging
+logging.basicConfig()
 log = logging.getLogger(__file__)
 
 
@@ -12,13 +13,18 @@ class Osm2pgsql(object):
     osm2pgsql_exe = None
     db_url = None
 
-    def __init__(self, db_url='', binary_name='osm2pgsql'):
+    def __init__(self, db_url='postgresql://ott@127.0.0.1:5432/ott', binary_name='osm2pgsql'):
         """
         """
         exe_path = exe_utils.find_executable(binary_name)
         if exe_path:
-            self.way_count = exe_utils
+            self.osm2pgsql_exe = exe_utils
             self.db_url = db_url
+            log.info("'{}' into '{}'".format(binary_name, db_url))
+            print exe_utils
+        else:
+            log.info("NOTE: this won't work, since can't find osm2pgsql binary '{}'".format(binary_name))
+
 
     def run(self):
         if self.db_url:
@@ -26,12 +32,12 @@ class Osm2pgsql(object):
 
     @classmethod
     def factory(cls, cfg='app.ini'):
+        self._config = ConfigUtil.factory(section=section)
         pass
 
 
 def main():
-    # import pdb; pdb.set_trace()
-    pass
+    Osm2pgsql()
 
 
 if __name__ == '__main__':
