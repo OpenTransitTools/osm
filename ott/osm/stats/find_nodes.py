f4ecd7cdd47a1cf39a88c740a04a8f76fb4f22dc
Frank Purcell <fxpurcell@gmail.com>
2018-04-23 17:50:53 -0700

    work more on find logicd

--- ott/osm/stats/find_nodes.py
+++ ott/osm/stats/find_nodes.py
@@ -8,23 +8,32 @@ import logging
 logging.basicConfig()
 log = logging.getLogger(__file__)
 
+class NodeInfo(object):
+    node = None
+    node_id = None
+    tag_name = None
+    tag_value = None
+
 
 class FindNodes(object):
     """
     Utility for finding OSM elements
     """
-    def __init__(self):
-        self.way_count = 0
-        self.highway_count = 0
+    def __init__(self, osm_file):
+        self.osm_file = osm_file
+        self.osm_path = osm_file # TODO
 
-
+    @classmethod
+    def find(cls, osm_file, tag_name, value_value=('yes', 'true')):
+        """ will find nodes with """
 
 
 def main():
     # import pdb; pdb.set_trace()
     from ott.utils.parse.cmdline import osm_cmdline
     p = osm_cmdline.osm_parser_args(prog_name='bin/osm_find', osm_required=True)
-    OsmInfo.print_stats(p.osm)
+    n = FindNodes.find(p.osm, p.tag_name)
+    print n
 
 
 if __name__ == '__main__':
