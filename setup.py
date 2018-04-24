343e0558faf47dae32423e9b417b6be688c39680
Frank Purcell <github@frankpurcell.com>
2018-04-23 21:36:31 -0700

    dependencies and data files

--- setup.py
+++ setup.py
@@ -42,7 +42,7 @@ setup(
     keywords='ott, osm, otp, gtfs, gtfsdb, data, database, services, transit',
     packages=find_packages(),
     package_data={
-        '': ['ott/osm/rename/config/*.csv']
+        'ott.osm': ['ott/osm/rename/config/*.csv']
     },
     include_package_data=True,
     zip_safe=False,
