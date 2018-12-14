3f138146c41cc917b35c63b1e0ab9bcb4dd2603e
Frank Purcell <fxpurcell@gmail.com>
2018-12-13 23:45:26 -0800

    have the OSM 2 PG loader running now agaist proper cache paths, etc...

---
+++ ott/osm/osm2pgsql/README.md
@@ -0,0 +1,50 @@
+osm2pgsql
+==
+**Downloads**
+1. x
+
+**???**
+
+
+Create Database for osm2pgsql
+===
+  - psql -d postgres -c "CREATE USER ott;"
+  - psql -d postgres -c "CREATE DATABASE osm WITH OWNER ott;"
+  - psql -d osm -c "CREATE EXTENSION postgis;"
+
+  + NOTE: assume's default postgres db exists 
+  + NOTE: assume's user has create db and user rights, and no uname/pass needed to run psql
+
+
+Styling
+===
+
+**2D and 3D Rendering**
+1. https://wiki.openstreetmap.org/wiki/Rendering
+1. https://github.com/tangrams/tangram
+1. https://gitter.im/tangrams/tangram-chat
+1. https://mapzen.com/documentation/tangram/Javascript-API/
+
+
+**Carto CSS**
+1. https://wiki.openstreetmap.org/wiki/CartoCSS
+
+
+**GeoServer, SLD and OSM:**
+1. https://docs.geoserver.org/stable/en/user/styling/index.html#styling
+
+1. https://gis.stackexchange.com/questions/156925/rendering-openstreetmaps-with-geoserver
+1. https://boundlessgeo.com/2015/11/openstreetmap-tile-cache-server-setup-and-load/
+1. https://github.com/boundlessgeo/OSM/
+1. https://github.com/dwins/mapnik2geotools
+
+1. https://gis.stackexchange.com/questions/256111/geoserver-osm-windows-sld-styling
+1. https://github.com/FlorentDotMe/archives/tree/master/2013.03.23-OSM-Styles-for-GeoServer
+1. https://github.com/mitchelljj/openstreetmap-sld
+
+1. https://live.osgeo.org/en/quickstart/mapproxy_quickstart.html
+
+
+MISC
+===
+1. https://github.com/NASAWorldWind
--- ott/osm/osm2pgsql/osm2pgsql.py
+++ ott/osm/osm2pgsql/osm2pgsql.py
@@ -1,6 +1,7 @@
 from ott.utils import exe_utils
 from ott.utils import file_utils
 from ott.utils import db_utils
+from ..osm_cache import OsmCache
 
 
 import logging
@@ -8,37 +9,33 @@ logging.basicConfig(level=logging.INFO)
 log = logging.getLogger(__file__)
 
 
-class Osm2pgsql(object):
-    """ run pgsql
+class Osm2pgsql(OsmCache):
+    """
+    run pgsql
     """
     osm2pgsql_exe = None
     db_url = None
     osm_path = None
 
-    def __init__(self, db_url='postgresql://ott@127.0.0.1:5432/ott', binary_name='osm2pgsql', osm_path='ott/osm/tests/data/portland.osm'):
+    def __init__(self, binary_name='osm2pgsql'):
         """
+        get paths to osm2pgsql binary, as well as (inheritted) paths to .osm file (from app.ini)
         """
-        exe_path = exe_utils.find_executable(binary_name)
-        if exe_path:
-            self.osm2pgsql_exe = exe_path
-            self.db_url = db_url
-            self.osm_path = osm_path
-            log.info("'{}' into '{}'".format(binary_name, db_url))
+        super(Osm2pgsql, self).__init__()
+
+        self.osm2pgsql_exe = exe_utils.find_executable(binary_name)
+        if self.osm2pgsql_exe and self.osm_path:
+            self.db_url = self.config.get('osm_url', section='db')
+            log.info("'{}' into '{}'".format(self.osm2pgsql_exe, self.db_url))
         else:
             log.info("NOTE: this won't work, since can't find osm2pgsql binary '{}'".format(binary_name))
 
     def run(self):
-        self.osm_path = 'or-wa.osm'
         if self.osm_path and self.db_url:
             db = db_utils.make_url(self.db_url)
             cmd = "{} -c -d {} -H {} -P {} -U {} {}".format(self.osm2pgsql_exe, db.database, db.host, db.port, db.username, self.osm_path)
             log.info(cmd)
             exe_utils.run_cmd(cmd, shell=True)
-
-    @classmethod
-    def factory(cls, cfg='app.ini'):
-        self._config = ConfigUtil.factory(section=section)
-        pass
 
 
 def main():
