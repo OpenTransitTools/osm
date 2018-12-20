a1dca7a1a2672463c01f73c0f9d190b07294d9dc
Frank Purcell <fxpurcell@gmail.com>
2018-12-19 17:49:08 -0800

    clean up code / document

--- ott/osm/osm2pgsql/osm2pgsql.py
+++ ott/osm/osm2pgsql/osm2pgsql.py
@@ -18,26 +18,26 @@ class Osm2pgsql(OsmCache):
     style_path = None
     epsg = "3857"
 
-    def __init__(self, binary_name='osm2pgsql'):
+    def __init__(self, binary_name="osm2pgsql"):
         """
         get paths to osm2pgsql binary, as well as (inheritted) paths to .osm file (from app.ini)
         """
         super(Osm2pgsql, self).__init__()
 
         # step 1: config the path to an osm2pgsql style file
-        style_file = self.config.get('osm2pgsql_style', default="default.style")
-        self.style_path = os.path.join(self.this_module_dir, 'styles', style_file)
+        style_file = self.config.get("osm2pgsql_style", def_val="default.style")
+        self.style_path = os.path.join(self.this_module_dir, "styles", style_file)
 
         # step 2: find the osm2pgsql binary ... if not found in your path, the db_url won't be set and nothing will run
         self.osm2pgsql_exe = exe_utils.find_executable(binary_name)
         if self.osm2pgsql_exe and self.osm_path:
-            self.db_url = self.config.get('url', section='osm_db')
+            self.db_url = self.config.get("url", section="osm_db")
             log.info("'{}' into '{}'".format(self.osm2pgsql_exe, self.db_url))
         else:
             log.warn("NOTE: this won't work, since can't find osm2pgsql binary '{}'".format(binary_name))
 
     def run(self):
-        min_size = self.config.get_int('min_size', def_val=100000)
+        min_size = self.config.get_int("min_size", def_val=100000)
         sized = file_utils.is_min_sized(self.osm_path, min_size)
         if sized and self.db_url:
             db = db_utils.make_url(self.db_url)
